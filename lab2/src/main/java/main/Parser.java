50e8a7e147368cda95801adc87acc5a83c1454dc
Alexander Kardash <129883094+Bel9shik@users.noreply.github.com>
2024-03-03 13:11:05 +0700

    Delete lab2 directory

--- lab2/src/main/java/main/Parser.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
-public interface Parser {
-    void loadCommands(ArrayList<String> list, BufferedReader reader) throws IOException;
-}
--- lab2/src/main/java/operations/Product.java
+++
@@ -1,8 +0,0 @@
-package operations;
-
-import java.util.Map;
-import java.util.Stack;
-
-public interface Product {
-    int doOperations(Map<String, Double> defines, Stack<Double> stack, String action);
-}
